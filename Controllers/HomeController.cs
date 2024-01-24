8af15b4ef2920de93e606213c58d5806effd7f2b
Alex Fang <106351391+alexfang324@users.noreply.github.com>
2024-01-24 13:30:49 -0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XSSDemo.ViewModels;
+
+namespace XSSDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)] // Disabling input validation to allow XSS
+        public ActionResult Index(FeedbackVM model)
+        {   // Directly passing user input to view
+            if (model.Comment.Contains("<script>"))
+            {
+                ViewBag.Comment = "Unsafe content seen";
+            }
+            else
+            {
+                ViewBag.Comment = HttpUtility.HtmlEncode(model.Comment);
+            }
+            return View();
+        }
+    }
+}
